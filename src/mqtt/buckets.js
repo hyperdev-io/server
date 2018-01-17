22f182d1b19266dbd6bff11f8de782aa345b02fe
jepee <jeroen@peetersweb.nl>
2018-01-17 15:01:37 +0100

    removed excessive space

--- src/mqtt/buckets.js
+++ src/mqtt/buckets.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 import {publishBuckets} from '../pubsub'
 
 module.exports = (BucketsDb) => (buckets) => {
-    for (let bucket of buckets) {        
+    for (let bucket of buckets) {
         BucketsDb.update({name: bucket.name}, {$set: bucket}, {upsert: true})
     }    
     BucketsDb.remove({name: {$nin: _.map(buckets, 'name')}})
