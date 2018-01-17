cf09ff6ff302af25be6c8b04f84e671abb5db85d
jepee <jeroen@peetersweb.nl>
2018-01-17 09:46:10 +0100

    republish apps after items are deleted

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -80,8 +80,10 @@ export const resolvers = {
     },
     removeApp: async (root, data, {db: {Apps}}) => {
       return new Promise((resolve, reject) => {
-        Apps.remove(_.pick(data, 'name', 'version'), {}, (err, numRemoved) => resolve(numRemoved))
-        pFindAll(Apps).then(docs => publishApps(docs))
+        Apps.remove(_.pick(data, 'name', 'version'), {}, (err, numRemoved) => {
+          resolve(numRemoved)
+          pFindAll(Apps).then(docs => publishApps(docs))
+        })
       })
     },
     startInstance: async (root, data, {db: {Instances, Apps}}) => {
