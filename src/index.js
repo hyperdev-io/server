574a07a297be53a78961940069daa3976d3af853
jepee <jeroen@peetersweb.nl>
2018-09-06 21:46:12 +0200

    initial auth

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { execute, subscribe } from 'graphql';
 const schema = require('./schema');
-
-
+const ldap = require('ldapjs');
+const uuidv1 = require('uuid/v1');
 const connectNedb = require('./nedb-connector');
 
 const PORT = 3010;
@@ -18,8 +18,14 @@ const CFG = {
     url: process.env.MQTT_URL,
     username: process.env.MQTT_USERNAME,
     password: process.env.MQTT_PASSWORD,
+  },
+  ldap: {
+    url: process.env.LDAP_URL,
+    bindDn: process.env.LDAP_BIND_DN,
+    adminDn: process.env.LDAP_ADMIN_DN,
+    adminPassword: process.env.LDAP_ADMIN_PASSWORD,
   }
-}
+};
 
 const start = async () => {
   const mqttClient = mqtt.connect(CFG.mqtt.url, {
@@ -36,6 +42,51 @@ const start = async () => {
   var app = express();
   app.use(cors())
   app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
+  app.post('/account/login', bodyParser.json({limit: '10kb'}), (request, response) => {
+    const credentials = request.body;
+    const client = ldap.createClient({ url: CFG.ldap.url });
+    client.bind(CFG.ldap.adminDn, CFG.ldap.adminPassword, (err, a,b,c) => {
+      response.setHeader('Content-Type', 'application/json');
+      var opts = {
+        filter: `(uid=${credentials.username})`,
+        scope: 'sub',
+        attributes: ['dn']
+      };
+      client.search(CFG.ldap.bindDn, opts, (err, res) => {
+        const dn = [];
+        res.on('searchEntry', entry => dn.push(entry.object.dn));
+        res.on('end', () => {
+          if(dn.length){
+            client.bind(dn[0], credentials.password, function(err) {
+              if( err ){
+                response.status(401);
+                response.end();
+              } else {
+                db.Accounts.findOne({dn: dn[0]}, (err, account) => {
+                  if(account === null){
+                    db.Accounts.insert({dn: dn[0]});
+                    db.AccessTokens.insert({dn: dn[0], token: uuidv1()})
+                  }
+                  db.AccessTokens.findOne({dn: dn[0]}, (err, at) => {
+                    response.end(JSON.stringify({token: at.token}));
+                  })
+                });
+              }
+            });
+          }else{
+            response.status(401);
+            response.end();
+          }
+        });
+      })
+
+      // console.log('admin login', err, a)
+    })
+    // client.bind(credentials.username, credentials.password, function(err, a,b,c) {
+    //   console.log('bind', err,a,b,c);
+    // });
+
+  });
   app.use('/graphql', bodyParser.json({ limit: '50mb' }), graphqlExpress({
     context: {db},
     schema,
--- src/nedb-connector.js
+++ src/nedb-connector.js
@@ -3,6 +3,8 @@ const Datastore = require('nedb')
 const db = {
   Instances: new Datastore(),
   Apps: new Datastore(({filename: './data/apps.db', autoload: true})),
+  Accounts: new Datastore(({filename: './data/accounts.db', autoload: true})),
+  AccessTokens: new Datastore(({filename: './data/tokens.db', autoload: true})),
   Buckets: new Datastore(),
   Resources: new Datastore(),
   DataStores: new Datastore(),
