7bf8918a7cc6e55aeb7be00b1aba2ba9eafea216
jepee <jeroen@peetersweb.nl>
2019-02-13 15:10:32 +0100

    map service labels to an array

--- src/mqtt/docker/swarm.js
+++ src/mqtt/docker/swarm.js
@@ -1,14 +1,17 @@
+import _ from 'lodash'
 import { publishInstances } from '../../pubsub'
 
-module.exports = (Instances) => (instances) => {
-  const ts = Date.now()
+export default (Instances) => (instances) => {
+  const ts = Date.now();
   for (let instName in instances) {
-    const instance = instances[instName]
-    instance._ts = ts
-    Instances.update({name: instName}, {$set: instance}, {upsert: true})
+    const instance = instances[instName];
+    instance._ts = ts;
+    for (let serviceName in instance.services) {
+      const service = instance.services[serviceName];
+      service.labels = _.toPairs(service.labels).map( ([label, value]) => ({ label, value }));
+    }
+    Instances.update({name: instName}, {$set: instance}, {upsert: true});
   }
-  Instances.remove({ $and: [{_ts: {$ne: ts}}, {state: {$ne: 'created'}}]})
-  Instances.find({}, (err, docs) => publishInstances(docs))
-  
+  Instances.remove({ $and: [{_ts: {$ne: ts}}, {state: {$ne: 'created'}}]});
+  Instances.find({}, (err, docs) => publishInstances(docs));
 };
-
